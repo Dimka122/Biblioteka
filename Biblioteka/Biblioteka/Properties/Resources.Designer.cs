74c5d0a90f3ac6ed44d8015e2e4562dceb810c70
Dimka122 <kassindima1@gmail.com>
2022-04-02 08:47:46 +0300

    \\\\

--- Biblioteka/Biblioteka/Properties/Resources.Designer.cs
+++ Biblioteka/Biblioteka/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Biblioteka.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap book {
+            get {
+                object obj = ResourceManager.GetObject("book", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap polka {
             get {
                 object obj = ResourceManager.GetObject("polka", resourceCulture);
